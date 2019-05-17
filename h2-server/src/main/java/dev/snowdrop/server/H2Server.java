9d8d717d893375138e76d79d470feda629864ebf
Charles Moulliard <cmoulliard@redhat.com>
2019-05-17 18:37:17 +0200

    Configure h2 web server

--- h2-server/src/main/java/dev/snowdrop/server/H2Server.java
+++ h2-server/src/main/java/dev/snowdrop/server/H2Server.java
@@ -11,7 +11,10 @@ public class H2Server {
         String[] params = new String[]{"-tcpPort", "8556", "-tcp", "-tcpAllowOthers", "-baseDir", "./h2-server","-ifNotExists","-trace"};
         try {
             Server s = Server.createTcpServer(params).start();
-            System.out.println(s.getStatus()); //prints out the server's status
+            System.out.println(s.getStatus());
+
+            Server webServer = Server.createWebServer("-webPort", "9090", "-webAllowOthers").start();
+            System.out.println(webServer.getStatus());
         } catch (Exception e) {
             e.printStackTrace();
         }
